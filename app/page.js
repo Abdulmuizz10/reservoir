0b217be6e1433b9cf905fd2db8c068bb901cd97c
Your Name <harrismuizz10@gmail.com>
2023-12-28 03:51:37 +0100

    adding a new page

--- app/page.js
+++ app/page.js
@@ -1,28 +1,10 @@
-"use client";
-import React, { useState } from "react";
-import Header from "@/components/Header/Header";
-import styles from "./page.module.css";
-import Hero from "@/components/Hero/Hero";
-import Footer from "@/components/Footer/Footer";
-import MeetingCard from "@/components/Meetingcard/MeetingCard";
-import MessageCard from "@/components/Messagecard/MessageCard";
-import { FaEnvelope } from "react-icons/fa6";
+import Everything from "@/page/Everything";
+import React from "react";
 
 export default function Home() {
-  const [popUp, setPopUp] = useState(false);
-  const [msgPopup, setMsgPopup] = useState(false);
   return (
-    <div style={{ position: "relative" }}>
-      <Header popUp={popUp} setPopUp={setPopUp} />
-      <Hero popUp={popUp} setPopUp={setPopUp} />
-      <Footer />
-      {popUp && <MeetingCard popUp={popUp} setPopUp={setPopUp} />}
-      {msgPopup && (
-        <MessageCard msgPopup={msgPopup} setMsgPopup={setMsgPopup} />
-      )}
-      <div className="fixedbutton" onClick={() => setMsgPopup(!msgPopup)}>
-        <FaEnvelope className="msgbutton" />
-      </div>
-    </div>
+    <>
+      <Everything />
+    </>
   );
 }
---
+++ page/Everything.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useState } from "react";
+import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
+import Footer from "@/components/Footer/Footer";
+import MeetingCard from "@/components/Meetingcard/MeetingCard";
+import MessageCard from "@/components/Messagecard/MessageCard";
+import { FaEnvelope } from "react-icons/fa6";
+
+export default function Everything() {
+  const [popUp, setPopUp] = useState(false);
+  const [msgPopup, setMsgPopup] = useState(false);
+  return (
+    <div style={{ position: "relative" }}>
+      <Header popUp={popUp} setPopUp={setPopUp} />
+      <Hero popUp={popUp} setPopUp={setPopUp} />
+      <Footer />
+      {popUp && <MeetingCard popUp={popUp} setPopUp={setPopUp} />}
+      {msgPopup && (
+        <MessageCard msgPopup={msgPopup} setMsgPopup={setMsgPopup} />
+      )}
+      <div className="fixedbutton" onClick={() => setMsgPopup(!msgPopup)}>
+        <FaEnvelope className="msgbutton" />
+      </div>
+    </div>
+  );
+}
